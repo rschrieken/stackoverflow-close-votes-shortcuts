e8369b314101bf800feec7278b123946404b67b6
rschrieken <rene.schrieken@gmail.com>
2014-10-08 23:47:16 +0200

    refactored code, simplified keyboard handling, js-lint-ed

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 // ==UserScript==
 // @version     0.4
 // @name        StackOverflow close votes shortcuts
@@ -10,120 +11,159 @@
 // @grant       none
 // ==/UserScript==
 
+/*global $:false, document */
+
 (function () {
-    var anotherSiteHandler = function (e) {
-        switch (e.keyCode) {
-            case 13: // ENTER - Vote
-                $('[type="submit"][value="Vote To Close"]').click()
-                $(document).off('keyup', anotherSiteHandler);
-                $(document).on('keyup', actionHandler);
-                break;
-            case 27: // ESC - Close Dialog
-                $(document).off('keyup', anotherSiteHandler);
-                $(document).on('keyup', actionHandler);
-                break;
-            case 49: // 1 - Meta
-                $('[name="migration"][value="meta.stackoverflow.com"]').click().focus();
-                break;
-            case 50: // 2 - SuperUser
-                $('[name="migration"][value="superuser.com"]').click().focus();
-                break;
-            case 51: // 3 - TEX
-                $('[name="migration"][value="tex.stackexchange.com"]').click().focus();
-                break;
-            case 52: // 4 - DBA
-                $('[name="migration"][value="dba.stackexchange.com"]').click().focus();
-                break;
-            case 53: // 5 - Stats
-                $('[name="migration"][value="stats.stackexchange.com"]').click().focus();
-                break;
-        }
-    };
-    var offTopicHandler = function (e) {
-        switch (e.keyCode) {
-            case 13: // ENTER - Vote
-                $('[type="submit"][value="Vote To Close"]').click()
-                $(document).off('keyup', offTopicHandler);
-                $(document).on('keyup', actionHandler);
-                break;
-            case 27: // ESC - Close Dialog
-                $(document).off('keyup', offTopicHandler);
-                $(document).on('keyup', actionHandler);
-                break;
-            case 49: // 1 - Super User
-                $('[name="close-as-off-topic-reason"][value="4"]').click().focus();
-                break;
-            case 50: // 2 - Server Fault
-                $('[name="close-as-off-topic-reason"][value="7"]').click().focus();
-                break;
-            case 51: // 3 - Recommend Resource
-                $('[name="close-as-off-topic-reason"][value="16"]').click().focus();
-                break;
-            case 52: // 4 - Debugging Help
-                $('[name="close-as-off-topic-reason"][value="13"]').click().focus();
-                break;
-            case 53: // 5 - Typografical Error
-                $('[name="close-as-off-topic-reason"][value="11"]').click().focus();
-                break;
-            case 54: // 6 - Another Site
-                $('[name="close-as-off-topic-reason"][value="2"]').click();
-                $(document).off('keyup', offTopicHandler);
-                $(document).on('keyup', anotherSiteHandler);
-                break;
-            case 55: // 7 - Other
-                $('[name="close-as-off-topic-reason"][value="3"]').click();
-                break;
-        }
-    };
-    var closeHandler = function (e) {
-        switch (e.keyCode) {
-            case 13: // ENTER - Vote
-                $('[type="submit"][value="Vote To Close"]').click()
-                $(document).off('keyup', closeHandler);
-                $(document).on('keyup', actionHandler);
-                break;
-            case 27: // ESC - Close Dialog
-                $(document).off('keyup', closeHandler);
-                $(document).on('keyup', actionHandler);
-                break;
-            case 49: // 1 - Duplicate
-                $('[name="close-reason"][value="Duplicate"]').click().focus();
-                $(document).off('keyup', closeHandler);
-                // TODO
-                break;
-            case 50: // 2 - Off Topic
-                $('[name="close-reason"][value="OffTopic"]').click().focus();
-                $(document).off('keyup', closeHandler);
-                $(document).on('keyup', offTopicHandler);
-                break;
-            case 51: // 3 - Unclear
-                $('[name="close-reason"][value="Unclear"]').click().focus();
-                break;
-            case 52: // 4 - Too Broad
-                $('[name="close-reason"][value="TooBroad"]').click().focus();
-                break;
-            case 53: // 5 - Opinion Based
-                $('[name="close-reason"][value="OpinionBased"]').click().focus();
-                break;
-        }
-    };
-    var actionHandler = function (e) {
-        switch (e.keyCode) {
-            case 49: // 1 - Leave Open
-                $('.review-actions [value="Leave Open"]').click();
-                break;
-            case 50: // 2 - Close
-                $('.review-actions [value="Close"]').click();
-                $(document).off('keyup', actionHandler);
-                $(document).on('keyup', closeHandler);
-                break;
-            case 51: // 3 - Edit
-                $('.review-actions [value="Edit"]').click();
-                break;
-            case 52: // 4 - Skip
-                $('.review-actions [value="Skip"]').click();
-                break;
-        }
-    };
-    $(document).on('keyup', actionHandler);
-})();
+	"use strict";
+
+	function select(name, value) {
+		return $('[name="' + name + '"][value="' + value + '"]');
+	}
+
+	function ClickFocus(name) {
+		return function (value) {
+			select(name, value).click().focus();
+		};
+	}
+
+	function Click(name) {
+		return function (value) {
+			select(name, value).click();
+		};
+	}
+
+	function ReviewClick() {
+		return function (value) {
+			$('.review-actions [value="' + value + '"]').click();
+		};
+	}
+
+	function close() {
+		$('[type="submit"][value="Vote To Close"]').click();
+	}
+
+    var state = 0,
+		anotherSiteHandler = function (e) {
+			var click = new ClickFocus('migration');
+			switch (e.keyCode) {
+			case 49: // 1 - Meta
+				click('meta.stackoverflow.com');
+				break;
+			case 50: // 2 - SuperUser
+				click('superuser.com');
+				break;
+			case 51: // 3 - TEX
+				click('tex.stackexchange.com');
+				break;
+			case 52: // 4 - DBA
+				click('dba.stackexchange.com');
+				break;
+			case 53: // 5 - Stats
+				click('stats.stackexchange.com');
+				break;
+			}
+		},
+		offTopicClick = new ClickFocus('close-as-off-topic-reason'),
+		offTopicClickOnly = new Click('close-as-off-topic-reason'),
+		offTopicHandler = function (e) {
+			switch (e.keyCode) {
+			case 49: // 1 - Super User
+				offTopicClick('4');
+				break;
+			case 50: // 2 - Server Fault
+				offTopicClick('7');
+				break;
+			case 51: // 3 - Recommend Resource
+				offTopicClick('16');
+				break;
+			case 52: // 4 - Debugging Help
+				offTopicClick('13');
+				break;
+			case 53: // 5 - Typografical Error
+				offTopicClick('11');
+				break;
+			case 54: // 6 - Another Site
+				offTopicClickOnly('2');
+				state = 3;
+				break;
+			case 55: // 7 - Other
+				offTopicClickOnly('3');
+				break;
+			}
+		},
+		closeClick = new ClickFocus('close-reason'),
+		closeHandler = function (e) {
+			switch (e.keyCode) {
+			case 49: // 1 - Duplicate
+				closeClick('Duplicate');
+				//switchKeyHandler(closeHandler);
+				//TODO
+				break;
+			case 50: // 2 - Off Topic
+				closeClick('OffTopic');
+				state = 2;
+				break;
+			case 51: // 3 - Unclear
+				closeClick('Unclear');
+				break;
+			case 52: // 4 - Too Broad
+				closeClick('TooBroad');
+				break;
+			case 53: // 5 - Opinion Based
+				closeClick('OpinionBased');
+				break;
+			}
+		},
+		reviewClick = new ReviewClick(),
+		actionHandler = function (e) {
+			switch (e.keyCode) {
+			case 49: // 1 - Leave Open
+				reviewClick('Leave Open');
+				break;
+			case 50: // 2 - Close
+				reviewClick('Close');
+				state = 1;
+				break;
+			case 51: // 3 - Edit
+				reviewClick('Edit');
+				break;
+			case 52: // 4 - Skip
+				reviewClick('Skip');
+				break;
+			}
+		};
+
+	// based on state call the correct handler
+	function handleStates(e) {
+		switch (state) {
+		case 0:
+			actionHandler(e);
+			break;
+		case 1:
+			closeHandler(e);
+			break;
+		case 2:
+			offTopicHandler(e);
+			break;
+		case 3:
+			anotherSiteHandler(e);
+			break;
+		default:
+			break;
+		}
+	}
+    $(document).on('keyup', function (e) {
+		switch (e.keyCode) {
+		case 13: // ENTER - Vote
+			close();
+			state = 0;
+			break;
+		case 27: // ESC - Close Dialog
+			state = 0;
+			break;
+		default:
+			handleStates(e); // select correct handler based on state
+			break;
+		}
+	});
+}());
